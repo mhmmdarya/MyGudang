6f5aced6e54f353a9e2888af515bf53203aa2c14
ImamWicaksono12 <mi8941609@gmail.com>
2023-05-18 23:07:07 +0700

    Membuat tampilan form edit barang dan tambah Barang

--- src/views/TambahBarang.java
+++ src/views/TambahBarang.java
@@ -4,6 +4,8 @@
  */
 package views;
 
+import DB.Supplier;
+
 /**
  *
  * @author ASUS
@@ -16,6 +18,7 @@ public class TambahBarang extends javax.swing.JFrame {
     public TambahBarang() {
         initComponents();
         setLocationRelativeTo(null);
+        setList();
     }
 
     /**
@@ -27,21 +30,58 @@ public class TambahBarang extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        listSupplier = new javax.swing.JComboBox<>();
+        jTextField1 = new javax.swing.JTextField();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        listSupplier.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listSupplierActionPerformed(evt);
+            }
+        });
+
+        jTextField1.setText("jTextField1");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(97, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(listSupplier, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 309, Short.MAX_VALUE))
+                .addGap(94, 94, 94))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addComponent(listSupplier, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(99, 99, 99)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(307, Short.MAX_VALUE))
         );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 500));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void listSupplierActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listSupplierActionPerformed
+        // TODO add your handling code here:
+        jTextField1.setText(listSupplier.getSelectedItem().toString());
+    }//GEN-LAST:event_listSupplierActionPerformed
+
+    public void setList() {
+        Object[][] listData = supplierModel.getList();
+        for (int i = 0; i < listData.length; i++) {
+            listSupplier.addItem((String) listData[i][1]);
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -77,7 +117,10 @@ public class TambahBarang extends javax.swing.JFrame {
             }
         });
     }
-
+    private Supplier supplierModel = new Supplier("supplier");
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JComboBox<String> listSupplier;
     // End of variables declaration//GEN-END:variables
 }
